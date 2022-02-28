488938e6d12e85a233def174f6dde25a3ee92064
Lucas Alves <alveslucaso094@gmail.com>
2022-02-28 17:16:04 -0300

    Style done

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,23 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Pesquise..." />
+        <input 
+        type="text" 
+        class="search-bar" 
+        placeholder="Pesquise..." 
+        />
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">São Paulo - SP</div>
+          <div class="date">Segunda 20/01/2022</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°C</div>
+          <div class="weather">Chuvoso</div>
+        </div>
       </div>
     </main>
   </div>
@@ -20,6 +36,7 @@
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap');
   * {
     margin: 0;
     padding: 0;
@@ -27,7 +44,7 @@
   }
 
   body {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-family: 'montserrat', sans-serif;
   }
 
   #app {
@@ -42,6 +59,11 @@
     padding: 25px;
 
     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+  }
+
+  .search-box {
+    width: 100%;
+    margin-bottom: 30px;
   }
 
   .search-box .search-bar{
@@ -65,5 +87,48 @@
     background: rgba(255, 255, 255, 0.75);
     box-shadow: 0 0 16px rgba(0, 0, 0, 0.25);
     border-radius: 16px 0 16px 0;
+  }
+
+  .location-box .location {
+    color: #FFF;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0 , 0.25);
+  }
+
+  .location-box .date {
+    color: #FFF;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+
+  .weather-box {
+    text-align: center;
+  }
+
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #FFF;
+    font-size: 102px;
+    font-weight: 900;
+
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background: rgba(255, 255, 255, 0.25);
+    border-radius: 16px;
+    margin: 30px 0;
+
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
+
+  .weather-box .weather {
+    color: #FFF;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
   }
 </style>
